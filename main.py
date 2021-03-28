6784a980606ec78ab4aaa616268820db64e81af3
David M <david.albazii@gmail.com>
2021-03-28 22:17:46 +0200

    moved addr comment

--- main.py
+++ main.py
@@ -3,14 +3,13 @@ import utime
 import sys
 from lib.lidar import LIDAR
 
+# TF-Luna has the default slave_address 0x10
 LIDAR_ADDRESS = 0x10
 
 i2c_0 = I2C(0, mode=I2C.MASTER, baudrate=400000, pins=('P7', 'P8'))
 utime.sleep_ms(50)
 
 slaves = i2c_0.scan()
-
-# TF-Luna has the default slave_address 0x10
 if LIDAR_ADDRESS not in slaves:
     print('Bus error: Please check LIDAR wiring')
     sys.exit()
@@ -18,7 +17,7 @@ if LIDAR_ADDRESS not in slaves:
 lidar_0 = LIDAR(i2c_0, LIDAR_ADDRESS)
 
 # Output limit when out of range
-# This case, output only when between 20cm and 150cm
+# Output only when between 20cm and 150cm (Up to 800cm)
 lidar_0.set_min_max(20, 150)
 
 
