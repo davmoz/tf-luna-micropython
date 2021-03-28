31866a57050e152bbb54eea0ecc78d86198fae75
David M <david.albazii@gmail.com>
2021-03-28 01:52:24 +0100

    changed wiring error statement

--- boot.py
+++ boot.py
@@ -3,27 +3,23 @@ import struct, utime
 import sys
 from lib.lidar import LIDAR
 
-
-
-
+LIDAR_ADDRESS = 0x10
 
 i2c_0 = I2C(0, mode=I2C.MASTER, baudrate=400000, pins=('P7', 'P8'))
 utime.sleep_ms(50)
 
-
 slaves = i2c_0.scan()
-if len(slaves) == 0:
-    print('Err in bus 0, please check wiring')
+
+# TF-Luna has the default slave_address 0x10
+if LIDAR_ADDRESS not in slaves:
+    print('Bus error: Please check LIDAR wiring')
     sys.exit()
 
-
-freq = 0x64 # 0x64 100Hz, 0xFA 250Hz
-lidar_0 = LIDAR(freq, i2c_0, slaves[0], 'Sens_0')
-
+lidar_0 = LIDAR(i2c_0, LIDAR_ADDRESS)
 
 while(True):
     s0 = lidar_0.get_distance()
     t0 = lidar_0.get_temp()
     a0 = lidar_0.get_signal_amp()
     print(s0, t0, a0)
-    utime.sleep_ms(50)
+    utime.sleep_ms(20)
--- lib/lidar.py
+++ lib/lidar.py
@@ -1,35 +1,41 @@
-from machine import I2C
-import utime, struct
+import struct
+import constants as const
 
 class LIDAR:
     """docstring for LIDAR."""
 
-    def __init__(self, freq, i2c, slave_addr, name):
+    def __init__(self, i2c, addr):
         self.i2c = i2c
-        self.slave_addr = "0x%X" % slave_addr
-        self.name = name
-        self.i2c.writeto_mem(int(self.slave_addr), 0x26, freq)
+        self.addr = int("0x%X" % addr)
+
+    def _save(self):
+        self.i2c.writeto_mem(self.addr, const.SAVE, 0x01)
 
     def get_addr(self):
-        return self.slave_addr
-
-    def get_name(self):
-        return self.name
+        return self.addr
 
     def get_distance(self):
-        dist = self.i2c.readfrom_mem(int(self.slave_addr), 0x00, 2)
-
+        dist = self.i2c.readfrom_mem(self.addr, 0x00, 2)
         return struct.unpack('<h', dist)[0]
 
     def get_temp(self):
-        temp = self.i2c.readfrom_mem(int(self.slave_addr), 0x04, 2)
-        return int(struct.unpack('<h', temp)[0]) * 0.01 
-    #
+        temp = self.i2c.readfrom_mem(self.addr, 0x04, 2)
+        return int(struct.unpack('<h', temp)[0]) * 0.01
+
     def get_signal_amp(self):
-        amp = self.i2c.readfrom_mem(int(self.slave_addr), 0x02, 2)
+        amp = self.i2c.readfrom_mem(self.addr, 0x02, 2)
         return int(struct.unpack('<h', amp)[0])
 
-    def sleep(self):
-        pass
-    def awake(self):
-        pass
+    def set_frequency(self, freq=0x64):
+        self.i2c.writeto_mem(self.addr, 0x26, freq)
+
+    def power_saving_mode(self, power_saving_mode=True):
+        val = 0x01 if power_saving_mode else 0x00
+        self.i2c.writeto_mem(self.addr, const.LOW_POWER, val)
+
+    def reboot(self):
+        self.i2c.writeto_mem(self.addr, const.SHUTDOWN_REBOOT, 0x02)
+
+    def on_off(self, on=True):
+        val = 0x00 if on else 0x01
+        self.i2c.writeto_mem(self.addr, const.ENABLE, val)
